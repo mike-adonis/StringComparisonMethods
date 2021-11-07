954b8716c3f71aee6474323c7c00482c40cb6e47
Mike <blakesmike12@gmail.com>
2021-11-07 11:08:52 +0100

    renamed test methods to follow  given-when-then convention

--- src/test/java/StringComparisonDemoTest.java
+++ src/test/java/StringComparisonDemoTest.java
@@ -14,13 +14,13 @@ class StringComparisonDemoTest {
 
     @Test
     @DisplayName("Test i1 and i3 are not equal")
-    void givenI1AndI3_ifI3NotStringObject_thenFail() {
+    void givenI1AndI3_ifBothNotStringObject_thenFail() {
         assertNotEquals(StringComparisonDemo.i1, StringComparisonDemo.i3);
     }
 
     @Test
     @DisplayName("Test that contentEquals method compares i1 and 13 correctly ")
-    void givenI1AndI3_ifBothObjectContentAreSame_thenSuccess() {
+    void givenI1AndI3_ifBothObjectCharactersAreSame_thenSuccess() {
         assertTrue(StringComparisonDemo.i1.contentEquals(StringComparisonDemo.i3));
     }
 
